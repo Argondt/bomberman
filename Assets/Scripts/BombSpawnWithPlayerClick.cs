d3b34868db6f8b5b7fe0f122cf0aadf5dba18838
Argondt <58610835+Argondt@users.noreply.github.com>
2021-11-10 21:45:16 +0100

    Add Colision and new layer with wall and observable

---
+++ Assets/Scripts/BombSpawnWithPlayerClick.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class BombSpawnWithPlayerClick : MonoBehaviour
+{
+    public Tilemap tilemap;
+    public GameObject gameObject;
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
