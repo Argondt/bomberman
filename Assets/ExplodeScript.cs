4e0147b53d3cf5d898e009861eefea15218fff5c
Krzysztof-Tomaszewski <krzysztof.tom@outlook.com>
2021-11-20 19:48:24 +0100

    Add bomb animation and player's explosions

--- Assets/ExplodeScript.cs
+++ Assets/ExplodeScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ExplodeScript : MonoBehaviour
 {
-    public float countdown2 = 2f;
+    public float countdown2 = 0.2f;
 
     // Start is called before the first frame update
     void Start()
@@ -19,8 +19,16 @@ public class ExplodeScript : MonoBehaviour
 
 		if (countdown2 <= 0f)
 		{
-            Debug.Log("Destroy explode");
+           // Debug.Log("Destroy explode");
 			Destroy(gameObject, 2);
 		}
 	}
+
+	void OnTriggerEnter2D(Collider2D collider)
+    {
+	   if(collider.tag == "Player" || collider.tag == "Player2") {
+			Debug.Log("Destroy player: "+ collider.tag);
+			Destroy(collider.gameObject);
+	   }
+    }
 }
